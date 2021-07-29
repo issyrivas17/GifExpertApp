1a61efc1a737499e0b8c581825e18537ca22512f
issyrivas17 <ismeniarivas17@gmail.com>
2021-07-28 22:33:26 -0400

    El boton no agrega la nueva categoria se debe revisar

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React,{useState}from 'react';
 
 const GifExpertApp = () => {  
+
+    /* const categories = ['One Punch','Samurai X','Dragon Ball'];  */
+const [categories, setCategories] = useState(['One Punch','Samurai X','Dragon Ball']); 
+
+const handleAdd = () => {
+setCategories([...categories,'Sailor Moon'])
+}
   return <> 
   <h2>GitExpertApp</h2> 
   <hr/> 
+  <button onClik={handleAdd}>Agregar</button>
+
+
+  <ol>
+   {categories.map( category => {
+       return <li key= {category}> {category}</li>
+   }
+    )}
+  </ol>
   </>; 
 
 }; 
