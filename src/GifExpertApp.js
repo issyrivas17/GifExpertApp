17dfbe5b8d719088424a9fb63bc6e85fb7981a63
issyrivas17 <ismeniarivas17@gmail.com>
2021-07-30 21:05:14 -0400

    Componente AddCategory

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,18 +1,19 @@
 import React,{useState}from 'react';
+import { AddCategory } from './components/AddCategory';
 
 const GifExpertApp = () => {  
 
-    /* const categories = ['One Punch','Samurai X','Dragon Ball'];  */
 const [categories, setCategories] = useState(['One Punch','Samurai X','Dragon Ball']); 
 
-const handleAdd = () => {
+/* const handleAdd = () => {
 setCategories([...categories,'Sailor Moon'])
 
-}
+} */
   return <> 
   <h2>GitExpertApp</h2> 
+  <AddCategory/>
   <hr/> 
-  <button onClick={ handleAdd }>Agregar</button>
+  
 
 
   <ol>
---
+++ src/components/AddCategory.js
@@ -0,0 +1,27 @@
+import React,{useState} from 'react'
+
+export const AddCategory = () => {
+
+    const [inputValue,setInputValue] = useState('Hola mundo')
+
+    const handleInputChange= (e) => {
+        setInputValue(e.target.value)
+    }
+
+    const handleSubmit = (e) => { 
+        e.preventDefault(); 
+console.log('Submit hecho')
+    }
+
+    return (
+        <form  onSubmit = {handleSubmit}> 
+        <input
+       type = "text" 
+       value = {inputValue} 
+       onChange = {handleInputChange} 
+       />
+        </form>
+      
+        
+    )
+}
