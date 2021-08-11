6bcfcf984478d6b02145cba04e17ca1c9d3250e5
issyrivas17 <ismeniarivas17@gmail.com>
2021-08-10 21:28:43 -0400

    Empezando a probar mi aplicacion GifExpertApp

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -2,15 +2,15 @@
     import { useFetchGifs } from '../Hooks/useFetchGifs';
     import {GifGridItem} from '../components/GifGridItem'
 
-
         export const GifGrid = ({category}) => {
 
         const {data:images,loading } =useFetchGifs(category); 
 
-
+        
             return (
             <>  
-                <h3 className="animate__wobble">{category}</h3>
+                <h3 className="animate__wobble">{category}</h3> 
+        
                 {loading && <p className="animate__flash"loading></p>}  
                 <div className = 'card-grid'>
             
